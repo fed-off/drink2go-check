b13d7ddf8db81c1d3d4cab3c41721ca83634ecdb
Aleksey Fedorov <138257434+fed-off@users.noreply.github.com>
2024-06-22 10:15:12 +0300

    Реализует мобильный header

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,8 @@
-/* в этот файл добавляет скрипты*/
+const menu = document.querySelector('.header__nav-list--menu');
+const menuToggle = document.querySelector('.header__nav-link--burger');
+
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('header__nav-link--burger-open');
+  menu.classList.toggle('header__nav-list--menu-open');
+  menu.classList.toggle('header__nav-list--menu-closed');
+});
